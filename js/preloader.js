d7d8058a8967058f16408f479f6c9752af80af6e
ᴄᴏᴅᴇʀ ᴀɴᴊɪ <92791382+cromaguy@users.noreply.github.com>
2024-05-04 08:44:18 +0530

    Added Animate CDN

--- js/preloader.js
+++ js/preloader.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $(".spinner-container"),setTimeout;
-    var table = $('#example').DataTable({
-      responsive: true
-    })
+$(document).ready(function () {
+  $(".spinner-container"), setTimeout;
+  var table = $('#example').DataTable({
+    responsive: true
+  })
     .columns.adjust()
     .responsive.recalc();
-    disableaccess(unsupportedmq);
-    })
-    setTimeout(() => {
-      detectInternet();
-        document.getElementById("spinner").style.display = "none";
-        document.getElementById("sidebar").style.display = "block";
-        document.getElementById("home-section").style.display = "block";
-        document.getElementById("dash-section").style.display = "block";
-      },900);
+  disableaccess(unsupportedmq);
+})
+setTimeout(() => {
+  detectInternet();
+  document.getElementById("spinner").style.display = "none";
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("home-section").style.display = "block";
+  document.getElementById("dash-section").style.display = "block";
+}, 900);
 
-    setTimeout(() => {
-        firstchart();
-        secondchart();
-        thirdchart();
-        fourthchart();
-        fifthchart();
-      },1800);
+setTimeout(() => {
+  firstchart();
+  secondchart();
+  thirdchart();
+  fourthchart();
+  fifthchart();
+}, 1800);
