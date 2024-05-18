d6bbf581cdc8521be0e7747eef1da1bc67e7e7c9
ᴄᴏᴅᴇʀ ᴀɴᴊɪ <92791382+cromaguy@users.noreply.github.com>
2024-05-18 08:04:47 +0530

    Fixed Notification Popup

--- js/collapse.js
+++ js/collapse.js
@@ -1,14 +1,32 @@
-var coll = document.getElementsByClassName("collapsible");
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.maxHeight) {
+//             content.style.maxHeight = null;
+//         } else {
+//             content.style.maxHeight = content.scrollHeight + "px";
+//         }
+//     });
+// }
+var coll = document.querySelectorAll(".collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
+
+        // Loop over all the panels an close each one
+        document.querySelectorAll(".content").forEach(function (content) {
+            content.style.maxHeight = "0";
+            content.previousElementSibling.classList.remove("active");
+        });
+
+        // Then show the clicked panel
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
+        content.style.maxHeight = content.scrollHeight + "px";
     });
 }
